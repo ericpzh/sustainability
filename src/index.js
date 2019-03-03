b14661202b433a15a40ebe34580c77ea4654e6e4
ericpzh <zp3@illinois.edu>
2019-03-02 22:53:29 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render((
     <Router>
         <div>
             <Route exact path="/sustainability/" component={App}/>
-            <Route exact path="/sustainability/table" component={Table}/>
+            <Route exact path="/sustainability/Table" component={Table}/>
         </div>
     </Router>
     ),
